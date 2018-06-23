ad539e267e97c1587f3ea9ef9b3d7e266cb342c1
lizmodern <lizmodern@gmail.com>
2018-06-23 13:13:08 +0300

    LPDEV-597

--- IATA/Modules/Authorization/Pin/Controls/PinViewController.swift
+++ IATA/Modules/Authorization/Pin/Controls/PinViewController.swift
@@ -25,7 +25,6 @@ class PinViewController: UIViewController {
         
         self.labelTitle.text = self.isValidation ? R.string.localizable.pinValidationTitle() : R.string.localizable.pinSetupTitle()
         
-        self.setNeedsStatusBarAppearanceUpdate()
         self.initNavBar()
     }
     
@@ -33,15 +32,7 @@ class PinViewController: UIViewController {
         return .default
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        self.initNavBar()
-        UIApplication.shared.statusBarStyle = UIStatusBarStyle.default
-    }
-    
-    
     private func initNavBar() {
-        self.setNeedsStatusBarAppearanceUpdate()
         self.navigationController?.navigationBar.barStyle = .black
         self.navigationController?.isNavigationBarHidden = false
         self.navigationItem.leftBarButtonItem = getBackButton()
