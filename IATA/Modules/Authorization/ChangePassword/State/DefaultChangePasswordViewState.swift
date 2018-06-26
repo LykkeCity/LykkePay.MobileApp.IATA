1656396e2b13239a46c0a2868917bd7f2c3b701c
Alexei Lazarev <a.lazarev@itransition.com>
2018-06-26 13:03:00 +0300

    fixed "The password is changed when New password is the same with Current password"

--- IATA/Modules/Authorization/ChangePassword/State/DefaultChangePasswordViewState.swift
+++ IATA/Modules/Authorization/ChangePassword/State/DefaultChangePasswordViewState.swift
@@ -4,7 +4,8 @@ import PromiseKit
 class DefaultChangePasswordViewState: DefaultBaseViewState, ChangePasswordViewState {
     
     func change(currentPassword: String, newPassword: String) -> Promise<Void>? {
-        return self.service.changePassword(oldPassword: currentPassword, newPassword: getHashPass(value: (newPassword + CredentialManager.shared.getUserName()!)))
+        return self.service.changePassword(oldPassword: getHashPass(value: (currentPassword + CredentialManager.shared.getUserName()!)),
+                                           newPassword: getHashPass(value: (newPassword + CredentialManager.shared.getUserName()!)))
     }
 
 }
