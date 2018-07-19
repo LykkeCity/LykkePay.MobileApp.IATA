2774c68d8704ded0fb5d21adae94132324dfd118
lizmodern <lizmodern@gmail.com>
2018-07-19 11:05:10 +0300

    updating separator

--- IATA/Library/Utils/Extensions/UITextFiledExtension.swift
+++ IATA/Library/Utils/Extensions/UITextFiledExtension.swift
@@ -3,11 +3,13 @@ import UIKit
 
 extension UITextField {
     internal func filterNumbers(with string: String) -> Bool {
-        let aSet = NSCharacterSet(charactersIn:"0123456789.").inverted
-        let compSepByCharInSet = string.components(separatedBy: aSet)
-        let numberFiltered = compSepByCharInSet.joined(separator: "")
-        if string != numberFiltered {
-            return false
+        if let separator = NSLocale.current.decimalSeparator {
+            let aSet = NSCharacterSet(charactersIn:"0123456789"+separator).inverted
+            let compSepByCharInSet = string.components(separatedBy: aSet)
+            let numberFiltered = compSepByCharInSet.joined(separator: "")
+            if string != numberFiltered {
+                return false
+            }
         }
         return true
     }
